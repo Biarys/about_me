8435503a6adb754165e8653a9afcd0897b5b1c2d
biarys <sbiarys@gmail.com>
2026-01-29 18:06:28 +0500

    added and configured vite-plugin-html to generate static html meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { PERSONAL_INFO } from "./src/constants/info";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,9 +14,23 @@ export default defineConfig(({ mode }) => ({
     },
   },
   base: "/about_me/",
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean,
-  ),
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: `${PERSONAL_INFO.firstName} | ${PERSONAL_INFO.title}`,
+          description: PERSONAL_INFO.bio.split(".")[0] + ".",
+          author: `${PERSONAL_INFO.firstName} ${PERSONAL_INFO.lastName}`,
+          ogTitle: `${PERSONAL_INFO.firstName} ${PERSONAL_INFO.lastName} | ${PERSONAL_INFO.title}`,
+          ogDescription: `${PERSONAL_INFO.bio.split(".")[0]}.`,
+          ogUrl: "https://biarys.github.io/about_me/",
+          ogImage: `/about_me/icon.svg`,
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
